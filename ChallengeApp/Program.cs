6a2928f60105704bd7f2ec606be15cf123207760
t j <fiat8mihi@gmail.com>
2023-05-19 18:15:38 +0200

    zadanie dzień czwarty

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, C# / .NET World!");
+﻿
+
+var name = "Tomek";
+bool female = false;
+var age = 3;
+
+
+if (female == true)
+{
+    if (age > 30)
+        if (name == "Ewa")
+            Console.WriteLine("Ewa lat 33");
+    if (age < 30)
+        Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
